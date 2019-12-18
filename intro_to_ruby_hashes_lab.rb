a4165303faf660db8c32cc6c2d3864913be7f2e6
weng lin <wenglin@buffalo.edu>
2019-12-18 00:42:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,9 @@ def monopoly_with_third_tier
 			:one_piece_owned=>25,
 			:two_pieces_owned=>50,
 			:three_pieces_owned=>100,
-			:four_pieces_owned=>200
-			}, :names
+			:four_pieces_owned=>200,
+			:names
+			},
 		}
 	}# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
