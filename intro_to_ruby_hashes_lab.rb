cd852f17f3268faa04f7ccdb8311ff1d941bcf3b
weng lin <wenglin@buffalo.edu>
2019-12-18 00:46:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
 				:reading_railroad => {},
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
-			 	:shortline => {}
+			 	:shortline_railroad => {}
 			}
 		}
 	}# When you start writing the implementation for this method, copy the Hash
