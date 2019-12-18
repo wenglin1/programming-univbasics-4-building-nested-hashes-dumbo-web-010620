eac0125efe10b9e15aef635d68a0a2aaba300309
weng lin <wenglin@buffalo.edu>
2019-12-18 00:46:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,8 @@ def monopoly_with_third_tier
 				:reading_railroad => {},
 				:learning_railroad => {},
 				:b_and_o_railroad => {},
-			 	:shortline => {}}
+			 	:shortline => {}
+			}				
 		}
 	}# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
