d851daca1dac1e2673e88675c668857c80122f27
weng lin <wenglin@buffalo.edu>
2019-12-18 00:43:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,7 @@ def monopoly_with_third_tier
 			:one_piece_owned=>25,
 			:two_pieces_owned=>50,
 			:three_pieces_owned=>100,
-			:four_pieces_owned=>200,
-			:names => {}
+			:four_pieces_owned=>200
 			}
 		}
 	}# When you start writing the implementation for this method, copy the Hash
