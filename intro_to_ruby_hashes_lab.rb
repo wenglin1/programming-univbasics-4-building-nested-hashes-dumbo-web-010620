7eabfe789c63167fcb483f3a625ef15b3799ac3d
weng lin <wenglin@buffalo.edu>
2019-12-18 00:49:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
 				:reading_railroad => {"mortgage_value"=>"$100"},
 				:pennsylvania_railroad => {"mortgage_value"=>"$200"},
 				:b_and_o_railroad => {"mortgage_value"=>"$400"},
-			 	:shortline_railroad => {"mortgage_value"=>"$300"}
+			 	:shortline_railroad => {"mortgage_value"=>"$800"}
 			}
 		}
 	}# When you start writing the implementation for this method, copy the Hash
